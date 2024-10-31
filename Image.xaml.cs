ee4fa6bb6ceac10609a72c7625628aaa45e2e282
María Rodríguez <lu.rzapien.92@gmail.com>
2024-10-31 15:52:52 -0600

    feat(image): Browser and load image

--- Image.xaml.cs
+++ Image.xaml.cs
@@ -27,10 +27,42 @@ namespace PhoneBook
         private void load_butt_Click(object sender, RoutedEventArgs e)
         {
             String fn;
-            fn = "C:\\Users\\pc-desarrollo\\Documents\\curso\\FirstWPFSn\\PhoneBook\\Data\\Pics\\pumpkin.jpeg";
+           fn = "C:\\Users\\pc-desarrollo\\Documents\\curso\\FirstWPFSn\\PhoneBook\\Data\\Pics\\autum.jpeg";
+           //fn = System.AppDomain.CurrentDomain.BaseDirectory+Data\\Pics\\autum.jpeg;
             //------------------------------------------
-            Uri img_path = new Uri(fn);
-            this.my_image.Source = new BitmapImage(img_path);
+            fn = this.text_box.Text;
+            //------------------------------------------
+            if(fn =="")
+            {
+                MessageBox.Show("File name is empty");
+            }
+            else
+            {
+                Uri img_path = new Uri(fn);
+                this.my_image.Source = new BitmapImage(img_path);
+            }
+           
+        }
+
+        private void fill_butt_Click(object sender, RoutedEventArgs e)
+        {
+            this.my_image.Stretch = Stretch.Fill;
+        }
+
+        private void zoom_butt_Click(object sender, RoutedEventArgs e)
+        {
+            this.my_image.Stretch = Stretch.Uniform;
+        }
+
+        private void browser_button_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog ofd = new Microsoft.Win32.OpenFileDialog();
+            //--------------------------
+            ofd.Title = "Select your image";
+            //
+            ofd.ShowDialog();
+            //-------------------------
+            this.text_box.Text = ofd.FileName;
         }
     }
 }
